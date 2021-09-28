64501f923897b1df654ac5a56c2e535b4204263f
Erick Concepcion <erick.concepcion@newfold.com>
2021-09-28 10:46:37 -0400

    Finish string table module

--- Program.fs
+++ Program.fs
@@ -6,17 +6,20 @@ open Utils
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
-
+type NameDsc = {Name: string; Dsc: string}
 [<EntryPoint>]
 let main argv =
-    let list: List<String[]> = [
-        [|"Name"; "Value"|]
-        [|"Erick"; "Hue"|]
-        [|"Cama"; "Objeto"|]
-        [|"Esto"; "Aquello"|]
-        [|"Mar"; "Icon"|]
+    
+    let list: List<NameDsc> = [
+        {Name ="Erick"; Dsc="Hue" }
+        {Name ="Cama"; Dsc="Objeto"}
+        {Name ="Esto"; Dsc="Aquello"}
+        {Name ="Mar"; Dsc="Icon"}
     ]
-    let tab = TableParser.DrawTableStruct list
+    let tab = TableParser.DrawStringTable [|"Name"; "Value"; "Concat Name+Value"|] [| 
+        (fun v -> v.Name)
+        (fun v -> v.Dsc)
+        (fun v -> $"{v.Name} {v.Dsc}") |] list
     printfn "%s" tab
     let key = Console.ReadKey()
     0 // return an integer exit code
--- Utils/tableparser.fs
+++ Utils/tableparser.fs
@@ -3,29 +3,53 @@ open System
 module TableParser =
     let DrawTableStruct (rows: List<String[]>)=
         let replicateStr str count = String.replicate count str
+        
+        let getMaxStringLength r = 
+            r |> Array.map String.length
+        
+        let getMaxColumnWiths arrValues = 
+            let colWidthFolder (colMax: int[]) (newArr: int[]) =
+                let moreOrEqual n1 n2 = if n1 >= n2 then n1 else n2
+                [| for i in 0 .. (Array.length colMax) - 1 -> moreOrEqual colMax.[i] newArr.[i] |]
+            let getMaxColWidths (lengths: List<int[]>) =
+                let length = Array.length lengths.Head
+                let lengthZeros: int array = Array.zeroCreate length
+                List.fold colWidthFolder lengthZeros lengths
+            arrValues |> List.map getMaxStringLength |> getMaxColWidths
+        
         let getRowSplitter arrValues =
             let getHeaderSpliter lengths = 
-                let sum3 i = i + 3
-                let minus m n = n-m
-                lengths |> List.sumBy sum3 |> minus 1 |> replicateStr "-"
-            let getMaxStringLength r = 
-                r |> Array.map String.length|> Array.max
-            arrValues |> List.map getMaxStringLength |> getHeaderSpliter
-        let fillStr fill str=
-            let minusNatural num1 num2 =
-                let res = num1-num2
-                if res >=0 then res else 0
-            str |> String.length |> minusNatural fill |> replicateStr str
+                lengths |> Array.sumBy (fun i -> i + 3) |> replicateStr "-"
+            arrValues |> getMaxColumnWiths |> getHeaderSpliter
+
+        let colWidths = getMaxColumnWiths rows
         let drawRowTableStruct row =
             let addPipes s = sprintf " | %s" s
-            row |> Array.map addPipes |> Array.reduce (+)
+            let expandCell  i (el: string) = el.PadRight(colWidths.[i])
+            row |> Array.mapi expandCell |> Array.map addPipes |> Array.reduce (+)
+
         let addSpliter spliter (strRows: List<string>) =
             let header = [strRows.Head; spliter]
-            List.append header strRows.Tail    
+            List.append header strRows.Tail
+
         let spliter = getRowSplitter rows
         let concatLines acc el = el |> sprintf "%s\n%s" acc
-        rows |> List.map drawRowTableStruct |> addSpliter spliter
+        rows |> List.map drawRowTableStruct
+        |> addSpliter spliter
         |> List.reduce concatLines
+    
+    let DrawStringTable colHeader (valueSelector:('b -> 'c)[]) values =
+        let rowCreator selectors value = Array.map (fun el -> $"{(el value)}") selectors
+        let writeTable (values: List<string[]>) = 
+            let (colLen, dataLen) = (Array.length values.Head, Array.length values.Tail.Head)
+            if colLen = dataLen then DrawTableStruct values 
+            else $"There are {colLen} definitions and the table needs {dataLen} according to the data"
+        values |> List.map (fun v -> rowCreator valueSelector v)
+        |> List.append [colHeader] 
+        |> writeTable
+
+        
+        
     
 
     
